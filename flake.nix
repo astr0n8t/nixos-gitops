5e9be28f837a92f21887e4726997c9ba95b04902
Nathan Higley <contact@nathanhigley.com>
2024-04-17 16:43:34 -0500

    feat: update comments

--- flake.nix
+++ flake.nix
@@ -3,12 +3,14 @@
   outputs = { self, ... }:
   {
 	buildNixOSGenerator = ({
+		# Define some sane defaults so hopefully the universe doesn't break
 		name ? "nixos",
 		system ? "x86_64-linux",
 		format ? "raw-efi",
 		modules ? [],
 		nixpkgs ? null,
 	}: let
+		# All this really does is output an attrset that matches what nixosGenerate expects
 		config = {
 			system = system;
 			format = format;
@@ -20,17 +22,24 @@
 			};
 		};
 	in
+		# Return the config
 		config
-	);
+	); 
 	buildNixOSConfig = (
 	node: let
+		# We can use the other function as a starting point
+		# which ensures we have some sane defaults 
 		generated = self.buildNixOSGenerator(node);
+		# Now we define the attrset that matches what nixosSystem expects
 		config = {
 			system = generated.system;
+			# Here is the secret sauce where we add our modified format file
+			# that adds the filesystems and bootloaders to our configuration
 			modules = generated.modules ++ [ ./formats/${node.format}.nix ];
 			specialArgs = generated.specialArgs;
 		};
 	in
+		# Return the config
 		config
 	);
   };
--- formats/sd-aarch64.nix
+++ formats/sd-aarch64.nix
@@ -1,3 +1,11 @@
+# sd-aarch64.nix
+# Duplicate nixox-generators' config so that we can use nixos-rebuild
+# https://github.com/nix-community/nixos-generators/blob/master/formats/sd-aarch64.nix
+# Fixes failed assertions with running nixos-rebuild:
+#   - The ‘fileSystems’ option does not specify your root file system.
+#   - You must set the option ‘boot.loader.grub.devices’ or 'boot.loader.grub.mirroredBoots' to make the system bootable.
+# In this case we need to use the generic-extlinux-compatible bootloader, not grub
+# Supposedly this only works for raspberry pi's; I built this for a Raspberry Pi 4 so mileage may vary but should work
 { config, pkgs, lib, ... }:
 {
   fileSystems."/" = {
