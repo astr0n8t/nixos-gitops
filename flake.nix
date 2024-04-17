119c150326a14bc76395b91c0098d32e8d3c80da
Nathan Higley <contact@nathanhigley.com>
2024-04-17 10:16:09 -0500

    fix: correct indentation

--- flake.nix
+++ flake.nix
@@ -2,25 +2,24 @@
   description = "a simple function provider to build attrsets for nixos-generators and nixos";
   outputs = { self, ... }:
   {
-  	  buildNixOSGenerator = (node: let
-		    config = {
-          system = node.platform ? "x86_64-linux";
-          format = node.format ? "raw-efi";
-          modules = node.modules ? null;
-          specialArgs = {
-              # additional arguments to pass to modules
-              self = self;
-              nodeHostName = node.name ? "nixos";
-              nixpkgs = node.nixpkgs ? null;
-            };
-        };
-      in
-			  config);
-      buildNixOSConfig = (node: let
-        format = node.format ? "raw-efi";
-			  node.modules = [ ./formats/${format}.nix ];
-			  config = self.buildNixOSGenerator(node);
-		  in
-			  config);
+	buildNixOSGenerator = (node: let
+		config = {
+			system = node.platform ? "x86_64-linux";
+			format = node.format ? "raw-efi";
+			modules = node.modules ? null;
+			specialArgs = {
+				self = self;
+				nodeHostName = node.name ? "nixos";
+				nixpkgs = node.nixpkgs ? null;
+			};
+		};
+	in
+		config);
+	buildNixOSConfig = (node: let
+		format = node.format ? "raw-efi";
+		node.modules = [ ./formats/${format}.nix ];
+		config = self.buildNixOSGenerator(node);
+	in
+		config);
   };
 }
