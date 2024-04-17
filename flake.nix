26baf3c6df363d0393b12a994e368f30748ba313
Nathan Higley <contact@nathanhigley.com>
2024-04-17 11:18:40 -0500

    fix: this should work

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
 	);
 	buildNixOSConfig = (
 	node: let
-		node.modules = [ (import ./formats/${node.format}.nix) ];
+		node.modules = [ import ./formats/${node.format}.nix ];
 		config = self.buildNixOSGenerator(node);
 	in
 		config
