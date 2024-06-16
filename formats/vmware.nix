b65c7cb9167e9643e28ee05491cfcf7042654177
Nathan Higley <contact@nathanhigley.com>
2024-06-15 22:20:57 -0500

    feat: support vmware format

---
+++ formats/vmware.nix
@@ -0,0 +1,29 @@
+# raw-efi.nix
+# Duplicate nixox-generators' config so that we can use nixos-rebuild
+# https://github.com/nix-community/nixos-generators/blob/master/formats/raw-efi.nix
+# Fixes failed assertions with running nixos-rebuild:
+#   - The ‘fileSystems’ option does not specify your root file system.
+#   - You must set the option ‘boot.loader.grub.devices’ or 'boot.loader.grub.mirroredBoots' to make the system bootable.
+{ config, pkgs, lib, ... }:
+{
+    fileSystems."/" = {
+      device = "/dev/disk/by-label/nixos";
+      autoResize = true;
+      fsType = "ext4";
+    };
+
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-label/ESP";
+      fsType = "vfat";
+    };
+
+    boot.growPartition = true;
+
+    boot.loader.grub = {
+      device = "nodev";
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+    };
+
+    virtualisation.vmware.guest.enable = true;
+}
